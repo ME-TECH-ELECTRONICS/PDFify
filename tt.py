a559d01f5f5ccd3b8ff6b4dad59245c33171189a
ME-TECH-ELECTRONICS <melvin.rt.123@gmail.com>
2024-11-18 07:41:50 +0530

    v1.0

--- tt.py
+++ tt.py
@@ -9,4 +9,15 @@ while True:
     merge_all = input("Merge all pdf [y/N]: ").lower() or "n"
     y1 = term.get_location()[0]
     while True:
-        
+        if msg == "M":
+            pattern = input("Order of pdf to merge comma seperated: ").strip()
+            if re.fullmatch(r"^\d+(,\d+)*$", pattern):
+                            pattern = list(map(int, pattern.split(",")))
+                            for i in range(len(pattern)):
+                                if 0 <= pattern[i]-1 < len(pdf_files): error = False
+                                else:
+                                    print("Please enter the valid file number!")
+                                    sleep(3)
+                                    rewrite_console_line(y1)
+                                    error = True
+                                    break
