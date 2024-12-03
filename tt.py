32c99b47eabce5119fa5a9d16a2709e303c1196e
ME TECH <melvin.rt.123@gmail.com>
2024-12-03 18:34:34 +0530

    v1.0

--- tt.py
+++ tt.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
+
+#imports
 import os
 import re
 import sys
 from typing import Callable
-
 import pikepdf
-import platform
 import subprocess
 from PIL import Image
 from tqdm import tqdm
@@ -12,20 +13,31 @@ from time import sleep
 from blessed import Terminal
 from PyPDF2 import PdfMerger, PdfReader, PdfWriter
 
+#Constants
 term = Terminal()
 OUTPUT_FOLDER = "output/"
 INPUT_FOLDER = "input/"
 MENUS = ["Convert to PDF", "Merge PDF", "Compress PDF", "Split PDF"]
 
+
 class BreakLoop(Exception):
+    """
+    Custom exception to break out of a loop.
+    """
     pass
 
+
 def check_app_install():
+    """
+   Checks if LibreOffice is installed and accessible via the terminal.
+
+   :return: True if LibreOffice is installed and accessible, False otherwise.
+   """
     try:
         subprocess.run(
-            ["libreoffice", "--version"], 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE, 
+            ["libreoffice", "--version"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
             check=True
         )
         return True
@@ -34,11 +46,23 @@ def check_app_install():
 
 
 def filter_valid_indices(arr1: list[int], arr2: list[int]) -> list[int]:
-    valid_indices = [index for index in arr2 if 0 <= index < len(arr1)]
-    return [arr1[index] for index in valid_indices]
-    
-    
+    """
+   Filters valid indices from `arr2` based on the bounds of `arr1`.
+
+   :param arr1: The original array.
+   :param arr2: The array of indices to filter.
+   :return: A list of valid indices from `arr2` within the bounds of `arr1`.
+   """
+    return [arr1[index] for index in arr2 if 0 <= index < len(arr1)]
+
+
 def parse_to_numbers(input_string: str) -> list[int]:
+    """
+   Parses a string into a list of numbers or ranges.
+
+   :param input_string: The input string to parse.
+   :return: A list of integers representing the parsed numbers or ranges.
+   """
     numbers = []
     matches = re.findall(r'(\d+)(?:-(\d+))?', input_string)
     for match in matches:
@@ -49,47 +73,55 @@ def parse_to_numbers(input_string: str) -> list[int]:
 
 
 def clear_console() -> None:
-    if platform.system() == "Windows":
+    """
+    Clears the console for better UI experience.
+    """
+    if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
+
 def check_folders() -> None:
-    if not os.path.exists(INPUT_FOLDER):
-        os.makedirs(INPUT_FOLDER)
-    if not os.path.exists(OUTPUT_FOLDER):
-        os.makedirs(OUTPUT_FOLDER)
+    """
+    Ensures the input and output folders exist, creating them if necessary.
+    """
+    os.makedirs(INPUT_FOLDER, exist_ok=True)
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 
 def rewrite_console_line(position: int) -> None:
     """
-    Moves the cursor up by `position` lines and clears those lines.
+    Overwrites the last `position` lines in the console.
 
-    :param position: The number of lines to move up from the current position.
+    :param position: Number of lines to overwrite
     """
-    if os.name == "nt":  # Windows
-        for _ in range(position):
-            sys.stdout.write("\033[F")  # Move up
-            sys.stdout.write("\033[K")  # Clear line
-    else:  # Linux/Unix
-        for _ in range(position):
-            sys.stdout.write("\033[F")  # Move up
-            sys.stdout.write("\033[K")  # Clear line
-    sys.stdout.flush()
+    for b in range(term.get_location()[0] - position):
+        sys.stdout.write("\033[F\033[K")
 
 
 def shorten_filename(filename: str, max_length: int = 12) -> str:
+    """
+    Shortens filenames to fit within a maximum length, adding ellipses if necessary.
+
+    :param filename: The original filename
+    :param max_length: The maximum length of the shortened filename
+    :return: The shortened filename
+    """
     name, ext = os.path.splitext(filename)
     if len(name) > max_length - len(ext) - 3:
         return name[:max_length - len(ext) - 3] + "..." + ext
     return filename
 
 
-def user_input_yn(prompt: str = "", default: str = "n") -> str:
-    return input(prompt).strip().lower() or default
-
-
 def user_input_option(prompt: str = "", default: int = 1) -> int:
+    """
+    Gets a numerical user input with a default value.
+
+    :param prompt: Shows the prompt
+    :param default: Default value
+    :return: integer option
+    """
     try:
         return int(input(prompt).strip() or default)
     except ValueError:
@@ -99,16 +131,24 @@ def user_input_option(prompt: str = "", default: int = 1) -> int:
 def print_title(action: str = "") -> None:
     print(term.white_on_blue(f"PDFify v1.0 | {action.upper()}"))
 
+
 def print_menu(disabled: list[int]) -> None:
-    
     for i, menu in enumerate(MENUS, start=1):
         if i not in disabled:
             print(term.gold(f"{i}. {menu}"))
         else:
             print(term.gray45(f"{i}. {menu}"))
     print(term.red("0. Exit"))
-            
+
+
 def user_input_range(prompt: str = "", default: list[int] = None) -> list[int]:
+    """
+    Gets a range of numerical user input.
+
+    :param prompt: Shows the prompt
+    :param default: Default value
+    :return: list of numbers
+    """
     if default is None:
         default = [1]
     user_input = input(prompt).strip()
@@ -118,24 +158,41 @@ def user_input_range(prompt: str = "", default: list[int] = None) -> list[int]:
     return filter_valid_indices(arr, default)
 
 
-def list_files(file_type: list[str]) -> list[str]:
-    return [file for file in os.listdir(INPUT_FOLDER) if file.endswith(tuple(file_type))]
+def list_files(file_types: list[str]) -> list[str]:
+    """
+    Lists files in the input folder matching given extensions.
+
+    :param file_types: list of file extensions
+    :return: list of files
+    """
+    return [file for file in os.listdir(INPUT_FOLDER) if file.endswith(tuple(file_types))]
 
 
-def docx_to_pdf(input_path: str, input_dir: str) -> str:
-    output_pdf = os.path.join(input_dir, os.path.basename(input_path).replace(".docx", ".pdf"))
+def docx_to_pdf(input_path: str) -> str:
+    """
+    Converts a DOCX file to PDF using LibreOffice.
+
+    :param input_path: path to the input docx
+    :return: path to the converted PDF
+    """
+    output_pdf = os.path.join(INPUT_FOLDER, os.path.basename(input_path).replace(".docx", ".pdf"))
     subprocess.run(
-        ["libreoffice", "--headless", "--convert-to", "pdf", input_path, "--outdir", input_dir],
+        ["libreoffice", "--headless", "--convert-to", "pdf", input_path, "--outdir", OUTPUT_FOLDER],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
     return output_pdf
 
 
-def ppt_to_pdf(input_path: str, output_dir: str) -> str:
-    output_pdf = os.path.join(output_dir, os.path.basename(input_path).replace(".pptx", ".pdf"))
+def ppt_to_pdf(input_path: str) -> str:
+    """
+    Converts a PPTX file to PDF using LibreOffice.
+    :param input_path: path to the input pptx
+    :return: path to the converted PDF
+    """
+    output_pdf = os.path.join(OUTPUT_FOLDER, os.path.basename(input_path).replace(".pptx", ".pdf"))
     subprocess.run(
-        ["libreoffice", "--headless", "--convert-to", "pdf", input_path, "--outdir", output_dir],
+        ["libreoffice", "--headless", "--convert-to", "pdf", input_path, "--outdir", OUTPUT_FOLDER],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
@@ -143,37 +200,41 @@ def ppt_to_pdf(input_path: str, output_dir: str) -> str:
 
 
 def image_to_pdf(input_path: str) -> str:
+    """
+    Converts an image file to PDF.
+
+    :param input_path: path to the input image
+    :return: path to the converted PDF
+    """
     img = Image.open(input_path)
     output_pdf = os.path.join(OUTPUT_FOLDER, os.path.basename(input_path).rsplit('.', 1)[0] + ".pdf")
     img.convert('RGB').save(output_pdf)
     return output_pdf
 
 
-def compress_pdf(input_pdf: str) -> str:
-    compressed_pdf = os.path.join(OUTPUT_FOLDER, "compressed_" + os.path.basename(input_pdf))
-    with pikepdf.open(input_pdf) as pdf:
-        pdf.save(compressed_pdf)
-    return compressed_pdf
+def compress_pdfs(input_pdfs: list[str]) -> str:
+    """
+   Compresses a PDF using `pikepdf`.
 
+   :param input_pdfs: List of paths to the input PDFs.
 
-def compress_pdfs(input_pdf: list[str]) -> None:
-    with tqdm(total=len(input_pdf), desc="Compressing PDFs") as progress_bar:
-        for file in input_pdf:
+   :return: Path to the compressed PDF.
+   """
+    with tqdm(total=len(input_pdfs), desc="Compressing PDFs") as progress_bar:
+        for file in input_pdfs:
             compressed_pdf = os.path.join(OUTPUT_FOLDER, "compressed_" + os.path.basename(file))
             with pikepdf.open(INPUT_FOLDER + file) as pdf:
                 pdf.save(compressed_pdf)
                 progress_bar.update(1)
+            return compressed_pdf
 
 
 def merge_pdfs(file_paths: list[str]) -> None:
     """
-    Merge multiple PDFs into a single PDF
+   Merges multiple PDFs into a single file.
 
-    Args:
-    :param file_paths:
-    :return:
-
-    """
+   :param file_paths: List of paths to the input PDFs.
+   """
     merger = PdfMerger()
     output_path = os.path.join(OUTPUT_FOLDER, "merged_output.pdf")
     with tqdm(total=len(file_paths), desc="Merging PDFs") as progress_bar:
@@ -182,7 +243,7 @@ def merge_pdfs(file_paths: list[str]) -> None:
             progress_bar.update(1)
         merger.write(output_path)
         merger.close()
-        compress_pdf(output_path)
+        compress_pdfs([output_path])
         os.remove(output_path)
 
 
@@ -190,12 +251,10 @@ def split_pdf(input_path: str, start_page: int, end_page: int, output_path: str)
     """
     Split a PDF into multiple PDFs
 
-    Args:
-    :param input_path: input path
-    :param start_page:
-    :param end_page:
-    :param output_path:
-    :return:
+    :param input_path: Path to the input PDF
+    :param start_page: Start page number
+    :param end_page: End page number
+    :param output_path: Path to the output PDF
     """
     reader = PdfReader(input_path)
     writer = PdfWriter()
@@ -209,9 +268,7 @@ def convert_and_compress_files(input_files: list[str]) -> None:
     """
     Convert and compress files
 
-    Args:
-        input_files (list[str]): List of file names
-
+    :param input_files: List of files to convert
     """
     total_files = len(input_files)
 
@@ -222,23 +279,23 @@ def convert_and_compress_files(input_files: list[str]) -> None:
             short_name = shorten_filename(filename)
             if ext.lower() == '.docx':
                 with tqdm(total=2, desc=f"Converting {short_name}", leave=False) as sub_bar:
-                    pdf_path = docx_to_pdf(file_path, OUTPUT_FOLDER)
+                    pdf_path = docx_to_pdf(file_path)
                     sub_bar.update(1)
-                    compress_pdf(pdf_path)
+                    compress_pdfs([pdf_path])
                     sub_bar.update(1)
                     os.remove(pdf_path)
             elif ext.lower() == '.pptx':
                 with tqdm(total=2, desc=f"Converting {short_name}", leave=False) as sub_bar:
-                    pdf_path = ppt_to_pdf(file_path, OUTPUT_FOLDER)
+                    pdf_path = ppt_to_pdf(file_path)
                     sub_bar.update(1)
-                    compress_pdf(pdf_path)
+                    compress_pdfs([pdf_path])
                     sub_bar.update(1)
                     os.remove(pdf_path)
             elif ext.lower() in ['.jpg', '.jpeg', '.png']:
                 with tqdm(total=2, desc=f"Converting {short_name}", leave=False) as sub_bar:
                     pdf_path = image_to_pdf(file_path)
                     sub_bar.update(1)
-                    compress_pdf(pdf_path)
+                    compress_pdfs([pdf_path])
                     sub_bar.update(1)
                     os.remove(pdf_path)
             else:
@@ -246,15 +303,16 @@ def convert_and_compress_files(input_files: list[str]) -> None:
             main_bar.update(1)
 
 
-def process_operation(menu_title: str, file_types: list[str], action: Callable, min_files: int = 1, error_message: str = "Enter at least one file to process"):
+def process_operation(menu_title: str, file_types: list[str], action: Callable, min_files: int = 1,
+                      error_message: str = "Enter at least one file to process"):
     """
     General function to process operations for a menu.
 
-    :param menu_title: Title to display for the current operation.
-    :param file_types: List of file extensions to filter.
-    :param action: Function to execute on selected files.
-    :param min_files: Minimum number of files required for the operation.
-    :param error_message: Message to display when user input is invalid.
+    :param menu_title: Menu title to display
+    :param file_types: List of file extensions to filter
+    :param action: Function to execute on selected files
+    :param min_files: Minimum number of files required for the operation
+    :param error_message: Error message to display if no files are selected
     """
     clear_console()
     print_title(menu_title.upper())
@@ -268,6 +326,7 @@ def process_operation(menu_title: str, file_types: list[str], action: Callable, 
     line = term.get_location()[0]
     try:
         while True:
+
             input_range = user_input_range(
                 f"Enter file number(s) to process [e.g., 1,3-5]: ",
                 [i for i in range(len(raw_files))]
@@ -283,11 +342,17 @@ def process_operation(menu_title: str, file_types: list[str], action: Callable, 
             else:
                 print("No files selected.")
                 sleep(3)
+                rewrite_console_line(line)
     except BreakLoop:
         pass
 
+
 if __name__ == "__main__":
     try:
+        if not check_app_install():
+            print("Libra Office is not installed")
+            sys.exit(0)
+        check_folders()
         while True:
             clear_console()
             print_title()
@@ -324,12 +389,12 @@ if __name__ == "__main__":
                     error_message="Enter at least one page to split"
                 )
             elif op == 0:
-                print(term.RED + "Quitting...")
-                sleep(3)
                 clear_console()
+                print(term.red("User interrupted. Quitting..."))
+                sleep(3)
                 exit()
     except KeyboardInterrupt:
-        print(term.RED + "Quitting...")
-        sleep(3)
         clear_console()
+        print(term.red("SIGINT received. Quitting..."))
+        sleep(3)
         exit()
